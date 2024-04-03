c15c665279dd5be6cea4fc8f0706123f13af7d90
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-04-03 22:00:27 +0530

    .
    Mind Twister is a simple yet addictive brain teaser game built using JavaScript. Challenge yourself and your friends with this interactive and entertaining game that will test your cognitive skills and logical reasoning. With its intuitive design and captivating gameplay, Mind Twister promises hours of fun and mental stimulation.
    
    Features:
    Brain Teasing Puzzles: Exercise your brain with a variety of challenging puzzles designed to test your problem-solving abilities and critical thinking skills. Each puzzle presents a unique challenge, keeping you engaged and entertained.
    Intuitive User Interface: Enjoy a seamless gaming experience with an intuitive user interface that makes navigation and gameplay a breeze. With easy-to-use controls and clear instructions, you can dive right into the game without any hassle.

---
+++ script.js
@@ -0,0 +1,83 @@
+function number() {
+    alert("you have to remember the digits")
+    alert("first digit is 7")
+
+    alert("second digit is 9")
+
+    alert("third digit is 5")
+
+    alert("fourth digit is 8")
+
+    alert("fifth digit is 1")
+
+    alert("sixth digit is 6")
+
+    alert("seventh digit is 4")
+
+    alert("eighth digit is 9")
+
+    alert("task1")
+    var user = prompt("enter the first five digits without giving space and without using commas")
+    if (user == 79581) {
+        alert("great")
+    } else {
+        alert("nice attemp but not correct")
+    };
+    alert("task 2")
+    var user2 = prompt("enter first five digits from end")
+    if (user2 == 94618) {
+        alert("awesome")
+    } else {
+        alert("oops! it is wrong")
+    };
+}
+//here code for hard challenge finished
+
+//code for medium challeng start
+
+function fruit() {
+    alert("remember fruits and their quantity")
+    alert("4 mangoes")
+
+    alert("3 apples")
+
+    alert("5 bananas")
+
+    alert("4 oranges")
+
+    alert("task 1")
+    var user = prompt("how many fruits are there except oranges")
+    if (user == 12) {
+        alert("nice")
+    } else {
+        alert("oops! incorrect")
+    }
+    alert("task2")
+    var user2 = prompt("how many pies you can make if 1 pie needs 1 mango,1apple and 2 bananas ")
+    if (user2 == 2) {
+        alert("great!")
+    } else {
+        alert("oops! incorrect")
+    };
+}
+//code for medium challenge finished
+
+//code for easy started
+function easy() {
+    alert("remember the birds")
+    alert(" 🐓 🐓 🦚 🦅  🦚  🦚 🐓🦅")
+    alert("task1")
+    var user = prompt("how many peacocks you have seen")
+    if (user == 3) {
+        alert("awesome")
+    } else {
+        alert("oops! incorrect")
+    }
+    alert("task 2")
+    var user2 = prompt("how many birds can fly")
+    if (user2 == 5) {
+        alert("you are great")
+    } else {
+        alert("oops! its wrong")
+    }
+}
